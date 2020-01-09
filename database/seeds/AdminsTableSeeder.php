7ebf81eb9a4b9ec0506408588de9c36b452c6a70
AungPhyoKywe <kophyokywe07@gmail.com>
2020-01-09 23:20:06 +0630

    fix

---
+++ database/seeds/AdminsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'superadmin',
+            'email' => 'hinata0777@gmail.com',
+            'password' => bcrypt('111111'),
+        ]);
+    }
+}
