141f700068692f4b7672e680348ee2c84798705c
AungPhyoKywe <kophyokywe07@gmail.com>
2020-01-22 17:29:44 +0630

    add middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -51,6 +51,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
+        'login'=>\App\Http\Middleware\LoginCheck::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
--- routes/web.php
+++ routes/web.php
@@ -14,27 +14,29 @@ Route::get('/',function (){
     return redirect('/login');
 });
 //superadmin
-Route::get('/backend','SuperAdminController@index');
+Route::group(['middleware' => ['login']], function() {
+    Route::get('/backend', 'SuperAdminController@index');
 
 
 //student
-Route::get('/student/getdata','StudentController@getData')->name('student.getData');
-Route::resource('class','ClassController');
-Route::resource('student','StudentController');
-Route::resource('teacher','TeacherController');
-Route::resource('timetable','TimetableController');
-Route::resource('enroll','EnrolmentController');
-Route::resource('subject','SubjectController');
-Route::resource('att','AttController');
-Route::resource('exam','ExamController');
-Route::get('/download/{file}','ExamController@download');
-Route::post('/search','AttController@search')->name('search');
-Route::get('/students/{id}','StudentController@destroy');
-Route::get('/teachers/{id}','TeacherController@destroy');
-Route::get('/timetables/{id}','TimetableController@destroy');
-Route::get('/enrolls/{id}','EnrolmentController@destroy');
-Route::get('/classes/{id}','ClassController@destroy');
+    Route::get('/student/getdata', 'StudentController@getData')->name('student.getData');
+    Route::resource('class', 'ClassController');
+    Route::resource('student', 'StudentController');
+    Route::resource('teacher', 'TeacherController');
+    Route::resource('timetable', 'TimetableController');
+    Route::resource('enroll', 'EnrolmentController');
+    Route::resource('subject', 'SubjectController');
+    Route::resource('att', 'AttController');
+    Route::resource('exam', 'ExamController');
+    Route::get('/download/{file}', 'ExamController@download');
+    Route::post('/search', 'AttController@search')->name('search');
+    Route::get('/students/{id}', 'StudentController@destroy');
+    Route::get('/teachers/{id}', 'TeacherController@destroy');
+    Route::get('/timetables/{id}', 'TimetableController@destroy');
+    Route::get('/enrolls/{id}', 'EnrolmentController@destroy');
+    Route::get('/classes/{id}', 'ClassController@destroy');
 
-Auth::routes();
+    Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+    Route::get('/home', 'HomeController@index')->name('home');
+});
