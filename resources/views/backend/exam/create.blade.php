ef14d1e2a51484868a8241643c412df2317b78dc
AungPhyoKywe <kophyokywe07@gmail.com>
2020-01-22 10:57:45 +0630

    fix

--- resources/views/backend/exam/create.blade.php
+++ resources/views/backend/exam/create.blade.php
@@ -17,57 +17,119 @@
                         <!-- /.card-header -->
                         <div class="card-body">
 
-
                             <div class="container-fluid">
                                 <div class="container-fluid">
                                     <section class="content">
                                         <!-- start content panel -->
                                         <div class="panel panel-inverse">
 
-                                            <!-- start content heading panel -->
-                                            <div class="panel-heading">
-
-                                            </div>
-                                            <!-- end content heading panel -->
-
-                                            <!-- start content body panel -->
-
                                             <div class="shadow-sm">
                                                 <div class="container">
 
                                                     <!-- start form -->
-                                                    <form name="studentForm" id="studentForm" method="POST" action="{{ route('exam.store') }}" data-parsley-validate="true" enctype="multipart/form-data">
+                                                    <form name="studentForm" id="studentForm" class="was-validated" method="POST" action="{{ route('exam.store') }}" data-parsley-validate="true" enctype="multipart/form-data" >
                                                         {!! csrf_field() !!}
 
                                                         <div class="row">
-                                                            <div class="col-xs-12 col-sm-12 col-md-6">
+                                                            <div class="col-xs-6 col-sm-12 col-md-6">
                                                                 <div class="form-group">
                                                                     <strong>Exam Name:</strong>
-                                                                    <select class="form-control"
+                                                                    <input class="form-control"
+                                                                            id="validationTooltip01"
                                                                             name="exam_name"
+                                                                           required
+                                                                            >
+                                                                    <div class="invalid-feedback">Example invalid feedback text</div>
+
+                                                                </div>
+                                                                    <div class="form-group">
+                                                                        <strong>Class:</strong>
+                                                                        <select class="form-control"
+                                                                               name="class_name"
+                                                                               id="customControlValidation2"
+                                                                               required
+                                                                               >
+                                                                            <option></option>
+                                                                        </select>
+                                                                        <div class="invalid-feedback">Example invalid feedback text</div>
+                                                                    </div>
+
+                                                                <div class="form-group">
+                                                                    <strong>Subject:</strong>
+                                                                    <select class="form-control"
+                                                                            name="subject_name"
+                                                                            id="customControlValidation2"
+                                                                            required
                                                                             data-parsley-required="true"
                                                                             data-parsley-maxlength="255">
-
-                                                                        {{--@foreach($teacher as $key =>$value)
-                                                                            <option value="{{ $value }}">{{$key}}</option>
-                                                                        @endforeach--}}
+                                                                        <option></option>
                                                                     </select>
+                                                                    <div class="invalid-feedback">Example invalid feedback text</div>
                                                                 </div>
-                                                            </div>
-                                                        </div>
 
+                                                                <strong>Upload Question:</strong>
+                                                                <div class="custom-file">
 
-                                                        <div class="row">
-                                                            <div class="col-xs-12 col-sm-12 col-md-6">
+                                                                    <input type="file" class="custom-file-input" id="validatedCustomFile" required>
+                                                                    <label class="custom-file-label" for="validatedCustomFile">Choose file...</label>
+                                                                    <div class="invalid-feedback">Example invalid custom file feedback</div>
+                                                                </div>
+                                                                </div>
+
+                                                            <div class="col-xs-6 col-sm-12 col-md-6">
                                                                 <div class="form-group">
-                                                                    <strong>Class Name:</strong>
+                                                                    <strong>Exam Date:</strong>
                                                                     <input class="form-control"
-                                                                           name="class_name"
+                                                                           type="date"
+                                                                            name="exam_name"
+                                                                            id="customControlValidation2"
+                                                                            required
+                                                                            data-parsley-required="true"
+                                                                            data-parsley-maxlength="255">
+                                                                    <div class="invalid-feedback">Example invalid feedback text</div>
+                                                                </div>
+
+
+                                                                <div class="form-group">
+                                                                    <strong>Start Time:</strong>
+                                                                    <input class="form-control"
+                                                                           type="time"
+                                                                           id="customControlValidation2"
+                                                                           required
+                                                                           name="start_time"
                                                                            data-parsley-required="true"
                                                                            data-parsley-maxlength="255">
+                                                                    <div class="invalid-feedback">Example invalid feedback text</div>
+
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <strong>End Time:</strong>
+                                                                    <input class="form-control"
+                                                                            type="time"
+                                                                            name="end_time"
+                                                                           id="customControlValidation2"
+                                                                           required
+                                                                            data-parsley-required="true"
+                                                                            data-parsley-maxlength="255">
+                                                                    <div class="invalid-feedback">Example invalid feedback text</div>
+                                                                </div>
+
+
+                                                                <div class="form-group">
+                                                                    <strong>Description:</strong>
+                                                                    <textarea class="form-control"
+                                                                              id="customControlValidation2"
+                                                                              required
+                                                                              name="description"
+                                                                              data-parsley-required="true"
+                                                                              data-parsley-maxlength="255"></textarea>
+                                                                    <div class="invalid-feedback">Example invalid feedback text</div>
                                                                 </div>
                                                             </div>
                                                         </div>
+
+
+
 
 
                                                         <div class="row">
