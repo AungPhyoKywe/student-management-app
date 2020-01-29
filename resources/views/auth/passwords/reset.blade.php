1f8c1ea2f970c5cdf63799228450ff6e58c84c4c
AungPhyoKywe <kophyokywe07@gmail.com>
2020-01-29 13:34:25 +0630

    fix reset blade file

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,6 +1,9 @@
 
 
 
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -21,43 +24,36 @@
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
-<body class="hold-transition login-page"style="background-color: #b3b7bb ">
+<body class="hold-transition login-page">
 <div class="login-box">
     <div class="login-logo">
-        <b>Schools</b>Management
+        <a href="../../index2.html"><b>Schools</b>Management</a>
     </div>
     <!-- /.login-logo -->
     <div class="card">
-        <div class="card-body">
+        <div class="card-body login-card-body">
             <p class="login-box-msg">You are only one step a way from your new password, recover your password now.</p>
 
             <form method="POST" action="{{ route('password.update') }}">
                 @csrf
-                <input type="hidden" name="token" value="{{ $token }}">
+                    <input type="hidden" name="token" value="{{ $token }}">
+                    <input id="email" type="hidden" placeholder="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
 
-                <div class="form-group row">
-
-                    <div class="input-group mb-4">
-
-                        <input id="email" type="email" placeholder="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-
-                        @error('email')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-                <div class="input-group mb-4">
+                <div class="input-group mb-3">
                     <input id="password" type="password" placeholder="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                    @error('password')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
+                        </div>
+                    </div>
                 </div>
-                <div class="input-group mb-4">
+                <div class="input-group mb-3">
                     <input id="password-confirm" placeholder="confirm password" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
+                        </div>
+                    </div>
                 </div>
                 <div class="row">
                     <div class="col-12">
@@ -65,17 +61,16 @@
                     </div>
                     <!-- /.col -->
                 </div>
-
-                </div>
             </form>
-                <p class="mt-3 mb-1">
-                    <a href="/">Login</a>
-                </p>
 
+            <p class="mt-3 mb-1">
+                <a href="/">Login</a>
+            </p>
         </div>
+        <!-- /.login-card-body -->
     </div>
 </div>
-
+<!-- /.login-box -->
 
 <!-- jQuery -->
 <script src="../../plugins/jquery/jquery.min.js"></script>
@@ -86,5 +81,7 @@
 
 </body>
 </html>
+
+
 
 
